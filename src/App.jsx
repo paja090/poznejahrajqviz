5c6add44100c994e32ef653289329a43014c77d1
paja090 <subert.pvel@gmail.com>
2025-11-17 22:49:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CreateRoom from "./pages/CreateRoom.jsx";
-import JoinRoom from "./pages/JoinRoom.jsx";
+import Home from "./pages/Home";
+import CreateRoom from "./pages/CreateRoom";
+import JoinRoom from "./pages/JoinRoom";
+import Lobby from "./pages/Lobby";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CreateRoom />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateRoom />} />
         <Route path="/join" element={<JoinRoom />} />
+        <Route path="/lobby/:roomCode" element={<Lobby />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
