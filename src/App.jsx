a5ee7c7ed0621f11dfb331d62c381f3529ac417d
paja090 <subert.pvel@gmail.com>
2025-11-17 22:58:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import CreateRoom from "./pages/CreateRoom";
 import JoinRoom from "./pages/JoinRoom";
 import Lobby from "./pages/Lobby";
+import Questions from "./pages/Questions";
 
 export default function App() {
   return (
@@ -12,7 +13,8 @@ export default function App() {
         <Route path="/create" element={<CreateRoom />} />
         <Route path="/join" element={<JoinRoom />} />
         <Route path="/lobby/:roomCode" element={<Lobby />} />
-      </Routes>
+        <Route path="/host/:roomCode/questions" element={<Questions />} />
+     </Routes>
     </BrowserRouter>
   );
 }
